16299ac794173f69f7f6937e3f7669df0a0ac613
brbnoriega <barbaranoriega330@gmail.com>
2022-11-16 19:59:10 -0300

    commit buttonGradient

--- weather-proyect/App.js
+++ weather-proyect/App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import ButtonGradient from './components/ButtonGradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <Text style={styles.titulo}>Hello</Text>
+      <Text style={styles.subTitle}>Sign In to your account</Text>
+      <TextInput
+        placeholder='weather@gmail.com'
+        style={styles.textInput}
+      ></TextInput>
+
+      <TextInput
+        placeholder='password'
+        style={styles.textInput}
+        secureTextEntry={true}
+      ></TextInput>
+
+      <Text style={styles.forgotPassword}>Forgot your password?</Text>
+      <ButtonGradient />
+      <Text style={styles.forgotPassword}>Dont have an account?</Text>
       <StatusBar style="auto" /> {/*modo white o dark */}
     </View>
   );
@@ -14,7 +30,7 @@ export default function App() {
 const styles = StyleSheet.create({  //clases para estilos de los componentes viene por default
   container: {
     flex: 1,
-    backgroundColor: '#fff', 
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
