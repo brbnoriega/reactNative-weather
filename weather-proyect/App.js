1fa02f40ad76c0b96405f793bd3e9877ac9cd109
brbnoriega <barbaranoriega330@gmail.com>
2022-11-29 20:19:40 -0300

    commit navitation

--- weather-proyect/App.js
+++ weather-proyect/App.js
@@ -1,32 +1,8 @@
 import React from "react";
-import { NativeRouter,  Switch, Route } from "react-native";
-import {Landing} from "./client/components/Landing/Landing";
-import Home from "./client/components/Home/Home";
-import Login from "./client/components/Login/Login";
-import {Profile} from "./client/components/Profile/Profile";
-import {EditProfile} from "./client/components/EditProfile/EditProfile";
-import {Detail} from "./client/components/Detail/Detail";
-import {ButtonGradient} from "./client/components/ButtonGradient/ButtonGradient";
-import { render } from "react-dom";
+import Navigation from "./Navigation";
 
-function App() {
-  
-  return (
-    //conocimiento del store le doy visibilidad a los componentes internos
-    <NativeRouter>
-      <Switch>
-  
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/login" component={Login} />
-
-       
-      </Switch>
-    </NativeRouter>
-
- 
-     
-  //holi
-  );
+export default function App(){
+  return(
+    <Navigation/>
+  )
 }
-
-export default App;
---
+++ weather-proyect/screens/SettingScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+
+const SettingScreen = () => {
+  return (
+   <View>
+    <Text
+    style={{
+        fontSize:30,
+        textAlign:'center',
+        marginTop: '20%'
+    }}
+    >SettingScreen</Text>
+   </View>
+  )
+}
+
+export default SettingScreen;
