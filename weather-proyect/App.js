125705601a7597b1cfab6f7512cb5cb3e9ccd4d0
MelHellrigl <hellriglandreamelissa@gmail.com>
2022-11-30 22:54:11 -0300

    Funciona Tab y Stack! :D

--- weather-proyect/App.js
+++ weather-proyect/App.js
@@ -1,35 +1,33 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import HomeScreen from './screens/HomeScreen';
-import SettingScreen from './screens/SettingScreen';
-import Detail from './screens/Detail';
-
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/HomeScreen";
+import SettingScreen from "./screens/SettingScreen";
+import Detail from "./screens/Detail";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 function App() {
   return (
-    <NavigationContainer> 
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
-      </Stack.Navigator>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        <Tab.Screen name="SettingScreen" component={SettingScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
-
 function Home() {
   return (
-    <Tab.Navigator>
-       <Tab.Screen name="HomeScreen" component={HomeScreen} />
-      <Tab.Screen name="Detail" component={Detail} />
-      <Tab.Screen name="SettingScreen" component={SettingScreen} />
-    </Tab.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="Detail" component={Detail} />
+      <Stack.Screen name="SettingScreen" component={SettingScreen} />
+    </Stack.Navigator>
   );
 }
-
 
 export default App;
